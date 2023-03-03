039ebdbfbd89ceea31382e54a5999b67e1a039cb
skillzo <skillzfire@yahoo.com>
2023-03-03 22:25:02 +0100

    initial commit using next js and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "blue-p": "#0064EB",
+      },
+    },
+  },
+  plugins: [],
+};
+
+// custom colors
+// blue-p = primary blue
